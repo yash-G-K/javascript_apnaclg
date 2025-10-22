6c3a4bc21c0560951aa3f463ec292ad8a2b81ac2
yashkalbhute6700 <yashk.cse22@sbjit.edu.in>
2025-10-23 01:01:16 +0530

    code changes

--- basics6.js
+++ basics6.js
@@ -1,56 +1,144 @@
-// functions in js
-// dice roll simulation
-function rollDice(){
-    return Math.floor(Math.random() * 6) + 1;
-}
+// // functions in js
+// // dice roll simulation
+// function rollDice(){
+//     return Math.floor(Math.random() * 6) + 1;
+// }
 
-console.log(rollDice());
+// console.log(rollDice());
 
-// function with arguments
-function greet(name){
-    console.log(`Hello, ${name}! Welcome to JavaScript.`);
-}
-greet("Yash");
+// // function with arguments
+// function greet(name){
+//     console.log(`Hello, ${name}! Welcome to JavaScript.`);
+// }
+// greet("Yash");
 
-// function with multiple arguments
-function add(a, b){
-    return a + b;
-}
-console.log(add(5, 10));
+// // function with multiple arguments
+// function add(a, b){
+//     return a + b;
+// }
+// console.log(add(5, 10));
 
-function table(number){
-    for(let i = 1; i <= 10; i++){
-        console.log(`${number} x ${i} = ${number * i}`);
+// function table(number){
+//     for(let i = 1; i <= 10; i++){
+//         console.log(`${number} x ${i} = ${number * i}`);
+//     }
+// }
+// console.log(table(73));
+
+// // concat of the string using function
+
+// let str = ["Hello", " ", "World", "!"];
+// function concat(str){
+//     let result;
+//     for(i = 0; i < str,length; i++){
+//         result += str[i];
+//     }
+//     return result;
+// }
+// console.log(concat(str));
+
+// // Scope in js
+// // scope defines the accessibility of variables,objects and functions in different parts of the code.
+// //function scope
+
+// function myFunction(){
+//     let functionScopeVar = "I am inside function";// function scope
+//     console.log(functionScopeVar);
+// }
+// myFunction();
+// // console.log(functionScopeVar); // error
+
+// // block scope
+// {
+//     let blockScopeVar = "I am inside block";
+//     console.log(blockScopeVar);
+// }
+// // console.log(blockScopeVar); // error
+
+// //lexical scope
+// // a variable defined outside a function can be accessed inside the function
+// function outerFunction(){
+//     let outerVar = "I am outside!";
+//     function innerFunction(){
+//         console.log(outerVar);
+//     }
+//     innerFunction();
+// }
+// outerFunction();
+
+// // function expression
+// const square = function(num){
+//     return num * num;
+// };
+// console.log(square(5));
+
+// let factorial = function fact(n){
+//     if(n <= 1) return 1;
+//     return n * fact(n - 1);
+// };
+// console.log(factorial(5));
+
+// High order functions
+// functions that take other functions as arguments or return functions as their results
+
+function multiplegreet(func , count){
+    for(let i = 0; i < count; i++){
+        func();
     }
 }
-console.log(table(73));
+let greet = function(){
+    console.log("Hello!");
+}
 
-// concat of the string using function
+multiplegreet(greet, 3);
 
-let str = ["Hello", " ", "World", "!"];
-function concat(str){
-    let result;
-    for(i = 0; i < str,length; i++){
-        result += str[i];
+// retruning function from function
+function oddoreven(request){
+    if(request === "even"){
+        let even = function(num){
+            console.log( num % 2 === 0);
+        }
+
+        return even;
+    } else if(request === "odd"){
+        let odd =  function(num){
+            console.log( num % 2 !== 0);
+        }
+        return odd;     
     }
-    return result;
-}
-console.log(concat(str));
+    return function(){
+        return null;
+    }
 
-// Scope in js
-// scope defines the accessibility of variables,objects and functions in different parts of the code.
-//function scope
-
-function myFunction(){
-    let functionScopeVar = "I am inside function";// function scope
-    console.log(functionScopeVar);
 }
-myFunction();
-// console.log(functionScopeVar); // error
+let evenFunction = oddoreven("even");
+evenFunction(4); // true
+let oddFunction = oddoreven("odd");
+oddFunction(5); // true
 
-// block scope
-{
-    let blockScopeVar = "I am inside block";
-    console.log(blockScopeVar);
-}
-// console.log(blockScopeVar); // error
+// Arrow functions
+// const add = (a, b) => {
+//     return a + b;
+// };
+// console.log(add(3, 7));
+
+//Methods in js
+const calculator = {
+    add : function(a,b) {
+        return a + b;
+    },
+    sub : function (a,b){
+        return a - b;
+    },
+    mul : function(a,b){
+        return a * b;
+    },
+    divide : function(a,b){
+        return a / b;
+    }
+
+};
+console.log(calculator.add(10, 5)); // 15
+console.log(calculator.sub(10, 5));
+console.log(calculator.mul(10, 5));
+console.log(calculator.divide(10, 5));
